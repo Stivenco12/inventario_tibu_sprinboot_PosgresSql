6490bd01b55d75465d9131eac25a6bb5058cf627
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-04-28 16:34:18 -0500

    ✨ Add Region entity with fields id, nameRegion, and relationship to Country

--- src/main/java/inventario_tibu/inventario_tibu/domain/entities/Country.java
+++ src/main/java/inventario_tibu/inventario_tibu/domain/entities/Country.java
@@ -1,10 +1,16 @@
 package inventario_tibu.inventario_tibu.domain.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
@@ -22,4 +28,7 @@ public class Country {
     @Column(name = "name_country",length = 50, nullable = false, unique = true)
     String nameCountry;
 
+    @OneToMany(mappedBy = "idCountry",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Set<Region> regions = new HashSet<>();
 }
