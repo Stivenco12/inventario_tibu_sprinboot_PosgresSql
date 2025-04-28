f42c4a1cc8235867acd2bb7045028f62bd7b4a90
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2025-04-28 15:02:07 -0500

    Initialize Spring Boot project with Maven wrapper and basic application structure

---
+++ src/main/java/inventario_tibu/inventario_tibu/InventarioTibuApplication.java
@@ -0,0 +1,13 @@
+package inventario_tibu.inventario_tibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioTibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioTibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inventario_tibu/inventario_tibu/InventarioTibuApplicationTests.java
@@ -0,0 +1,13 @@
+package inventario_tibu.inventario_tibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioTibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
